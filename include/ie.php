a5c0653e4eb020c6276a49657739f6bd099e9048
blade1989 <imri.paloja@gmail.com>
2014-01-05 00:45:00 +0100

    improved format

--- include/ie.php
+++ include/ie.php
@@ -12,19 +12,23 @@
   <!--[if lt IE 7]> 
 
 <ul id="outdated">
-  <li>
-    <a href="http://mozilla.org/firefox" title="Mozilla Firefox">
-      <img src="http://upload.wikimedia.org/wikipedia/commons/f/ff/Mozilla_Firefox_logo_2013.png" style="width:45px;" alt="firefox"><br>
-      Mozilla Firefox</a><br>
+	<li>
+    	<a href="http://mozilla.org/firefox" title="Mozilla Firefox">
+      		<img src="http://upload.wikimedia.org/wikipedia/commons/f/ff/Mozilla_Firefox_logo_2013.png" style="width:45px;" alt="firefox"><br>
+      Mozilla Firefox</a>
+      <br>
 
-      <p class="description">Most widely used open-source browsers, easily expandable and adjust as needed</p>
+      	<p class="description">Most widely used open-source browsers, easily expandable and adjust as needed</p>
       
       <div class="company" style="width:125px;">
         Mozilla <a href="https://www.mozilla.org/" title="Mozilla foundation">Foundation</a>/<a href="http://www.mozilla.com/" title="Mozilla Corporation">Corporation</a>
         <a href="http://www.mozilla.org/credits/" title="Mozilla Contributors">and contributors</a>
 
-        <p><small>Developer(s)</small></p></div>
-    </li>
+        <p>
+	        <small>Developer(s)</small>
+        </p>
+       </div>
+	</li>
 
   <li>
     <a href="http://www.google.com/chrome" title="Google Chrome">
@@ -33,7 +37,13 @@
 
       <p class="description">Google's browser with intuitive interface. Automatically always up-to-date!</p>
 
-      <div class="company"><a href="https://www.google.nl/" title="Goolge">Google inc.</a><br><p><small>Developer(s)</small></p></div>
+      <div class="company">
+	      <a href="https://www.google.nl/" title="Goolge">Google inc.</a><br>
+
+	      	<p>
+	      		<small>Developer(s)</small>
+	      	</p>
+	      </div>
     </li>
 
   <li>
@@ -43,6 +53,12 @@
 
       <p class="description">Browser with many advanced features, free alternative</p>
 
-      <div class="company"><a href="http://www.opera.com/" title="Opera">Opera Software</a><br><p><small>Developer(s)</small></p></div>
+      <div class="company">
+      <a href="http://www.opera.com/" title="Opera">Opera Software</a><br>
+
+      	<p>
+      		<small>Developer(s)</small>
+      	</p>
+      </div>
     </li>
 </ul> <![endif]--> 
