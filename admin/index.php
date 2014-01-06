df290b1bfafb32506d0f8b2bf0e906d13a3dc592
blade1989 <imri_paloja@hotmail.com>
2014-01-06 09:41:27 +0100

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -13,51 +13,22 @@ if (!USERNAME or isset($_GET['logout']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Masters House - Imri Paloja</title>
-<link rel="stylesheet" type="text/css" href="css/framework.css">
-<link rel="stylesheet" type="text/css" href="/css/flexslider.css">
+<title><?php $title ?> - <?php $sitename ?></title>
+
+<?php include './include/css.php'; ?>
+
 <link rel="stylesheet" type="text/css" href="css/admin.css">
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-<meta name="description" content="Admin panel">
-<meta name="keywords" content="Masters House, Bow down.">
-<meta name="author" content="Imri Paloja" >
-<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta name="description" content="<?php $description ?>">
+<meta name="keywords" content="<?php $keywords ?>">
+<meta name="author" content="<?php $author ?>" >
+<meta charset="utf-8">
 
-<!-- Add jQuery library -->
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<?php include './include/fancybox.php'; ?>
 
-<!-- Add mousewheel plugin (this is optional) -->
-<script type="text/javascript" src="/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+<?php include './include/ie.php'; ?>
 
-<!-- Add fancyBox -->
-<link rel="stylesheet" href="/fancybox/source/jquery.fancybox.css" type="text/css" media="screen">
-<script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js"></script>
-
-<!-- Optionally add helpers - button, thumbnail and/or media -->
-<link rel="stylesheet" href="/fancybox/source/helpers/jquery.fancybox-buttons.css" type="text/css" media="screen">
-<script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-buttons.js"></script>
-<script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-media.js"></script>
-
-<link rel="stylesheet" href="/fancybox/source/helpers/jquery.fancybox-thumbs.css" type="text/css" media="screen">
-<script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-thumbs.js"></script>
-
-<!-- scripts -->
-<script type="text/javascript" src="/js/scripts.js"></script>
-<script type="text/javascript" src="/js/jquery.flexslider.js"></script>
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-36882486-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
+<?php include './include/js.php'; ?>
 
 </head>
 
