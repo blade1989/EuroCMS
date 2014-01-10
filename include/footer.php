86418bb5713f3338d5be1bbbb61eb501922fef95
blade1989 <imri.paloja@gmail.com>
2014-01-10 19:58:24 +0100

    little bit of editing

--- include/footer.php
+++ include/footer.php
@@ -1,23 +1,9 @@
 <?php defined('DS') OR die('No direct access allowed.');?>
 
 <div id="license">
-  <!-- <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
-    <img alt="Creative Commons License" style="border-width:0;width:75px;" src="http://i.creativecommons.org/l/by-sa/4.0/80x15.png">
-      </a>
-      <br> -->
-  <p>
-    License: <a href="http://creativecommons.org/licenses/by-sa/4.0/"> 
-        <abbr title="Attribution-ShareAlike 4.0 International">CC BY-SA 4.0</abbr></a> 
-        <a href="http://www.eurobytes.nl/" title="EuroBytes">EuroBytes</a>
 
-  |
-
-  EuroBytes theme by <a href="https://plus.google.com/+ImriPaloja/posts" rel="author" title="Author">Imri Paloja</a>
-
-  <!-- |
-
-  Cookies: -->
-
+<p>
+  <a href="https://plus.google.com/+ImriPaloja/posts" rel="author" title="Author">Imri Paloja</a>
 </p>
 
 </div>
